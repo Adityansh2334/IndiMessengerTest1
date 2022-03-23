2b4f2d2d5a13ba4610d40b7a4ac63cb2599552dc
adityabehera23@gmail.com <adityabehera23@gmail.com>
2022-03-24 00:10:15 +0530

    Initial Setup for Messaging Web App

---
+++ src/main/java/com/test/indimesenger/IndiMesengerApplication.java
@@ -0,0 +1,13 @@
+package com.test.indimesenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndiMesengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndiMesengerApplication.class, args);
+    }
+
+}
